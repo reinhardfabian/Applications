3b3f5e8171b3f3bc5a15ded5ff4afca0b28a73a4
Reinhard Fabian <reinhard.fabian@gmail.com>
2023-08-06 08:30:57 +0200

    bette code structur

--- scripts/main.js
+++ scripts/main.js
@@ -11,53 +11,53 @@ const companies = [
   ...
 ];
 */
+const table = document.getElementById('company-table');
+document.getElementById('caption').textContent = `${companies.length} applications`;
+const tableBody = table.createTBody();
 
-function fillTable() {
-  const table = document.getElementById('company-table');
-  document.getElementById('caption').textContent = `${companies.length} applications`;
-  const tableBody = table.createTBody();
-
-  //fill table head
-  const tableHead = table.createTHead();
-  let tr = tableHead.insertRow(0);
-  for (const property in companies[0]) {
-    let th = document.createElement('th');
-    th.textContent = property.replace(/^\w/, m => m.toUpperCase());
-    th.setAttribute('id', property);
-    th.addEventListener('click', () => fillTableBody(property));
-    tr.appendChild(th);
-  }
-
-  //function to fill table body via sort criteria (column)
-  function fillTableBody(column) { //local function
-    // sort list
-    if (column == 'date') {
-      companies.sort( (a,b) => a.date - b.date );
-    } else {
-      companies.sort( (a,b) => a[column].localeCompare(b[column]) );
-    }
-    // erase table body
-    while (tableBody.firstChild !== null) {
-      tableBody.removeChild(tableBody.firstChild);
-    }
-    // generate table body
-    let i = 0; // i is table row‚
-    for (const item of companies) {
-      let j = 0; // j is table column
-      tr = tableBody.insertRow(i++);
-      for ( const property in companies[0] ) {
-        if (property == 'date'){
-          tr.insertCell(j++).textContent = item.date.toLocaleDateString();
-        } else {
-          tr.insertCell(j++).textContent = item[property];
-        }
-        // debugger;
-      }
-      if (i % 2) tr.setAttribute('class', 'row even');  //pretty print
-    }
-  }
-
-  fillTableBody('date'); // fill table - date is initial sort
+//fill table head
+const tableHead = table.createTHead();
+let tr = tableHead.insertRow(0);
+for (const property in companies[0]) {
+  let th = document.createElement('th');
+  th.textContent = property.replace(/^\w/, m => m.toUpperCase());
+  th.setAttribute('id', property);
+  th.addEventListener('click', () => sortTableBody(property));
+  tr.appendChild(th);
 }
 
-document.addEventListener('DOMContentLoaded', fillTable);
+//function to fill table body via sort criteria (column)
+function sortTableBody(property) { //local function
+  // sort list
+  if (property == 'date') {
+    companies.sort((a, b) => a.date - b.date);
+  } else {
+    companies.sort((a, b) => a[property].localeCompare(b[property]));
+  }
+  // erase table body
+  while (tableBody.firstChild !== null) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+  printTableBody(property)
+}
+
+function printTableBody(){
+  // generate table body
+  let i = 0; // i is table row‚
+  for (const item of companies) {
+    let j = 0; // j is table column
+    tr = tableBody.insertRow(i++);
+    for (const property in companies[0]) {
+      if (property == 'date') {
+        tr.insertCell(j++).textContent = item.date.toLocaleDateString();
+      } else {
+        tr.insertCell(j++).textContent = item[property];
+      }
+      // debugger;
+    }
+    if (i % 2) tr.setAttribute('class', 'row even');  //pretty print
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', printTableBody);
