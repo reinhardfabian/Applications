4ad5314ee73290604cfa3dc54ecf1ee186a0f587
Reinhard Fabian <reinhard.fabian@gmail.com>
2023-08-12 13:41:57 +0200

    add input animation

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 document.getElementById('caption').textContent = `${companies.length} applications`;
 
-const trHead = document.getElementById('trhead'); 
+const trHead = document.getElementById('trhead');
 for (const property in companies[0]) {
   let th = document.createElement('th');
   th.textContent = property.replace(/^\w/, m => m.toUpperCase());
@@ -14,19 +14,19 @@ for (const property in companies[0]) {
 const tableBody = document.getElementById('tablebody');
 
 function printTableBody(column) {
- companies.sort( (column == 'date') ? (a, b) => a.date - b.date : (a, b) => a[column].localeCompare(b[column]) );
+  companies.sort((column == 'date') ? (a, b) => a.date - b.date : (a, b) => a[column].localeCompare(b[column]));
   // erase table body
   while (tableBody.firstChild !== null) {
     tableBody.removeChild(tableBody.firstChild);
   }
   // generate table body
-  for (let i=0; i<companies.length; i++) {
+  for (let i = 0; i < companies.length; i++) {
     const tr = tableBody.insertRow();
     for (const property in companies[0]) {
-      tr.insertCell().textContent = (property == 'date') ? companies[i].date.toLocaleDateString() : companies[i][property] ;
+      tr.insertCell().textContent = (property == 'date') ? companies[i].date.toLocaleDateString() : companies[i][property];
       // debugger;
     }
-    if (!(i%2)) tr.setAttribute('class', 'even');  //pretty print
+    if (!(i % 2)) tr.setAttribute('class', 'even');  //pretty print
   }
 }
 
