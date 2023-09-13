1400dcc2d3019f8cbcf4f1f9d0bc6a3c4295b5d9
Reinhard Fabian <reinhard.fabian@gmail.com>
2023-09-13 15:38:52 +0200

    activated eslinter check

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,49 +1,44 @@
-const fetchURL = 'companies.json';
-
-async function printTable(event: Event) {
-  // console.log(event?.type);
-  interface Company {
-    date: string;
-    name: string;
-    job: string;
-    location: string;
-    success: string;
-  }
-  type Property = keyof Company;
-  const response = await fetch(fetchURL);
-  const data = await response.json();
-  const companies: Company[] = data.companies;
-  const properties = Object.keys(companies[0]) as Property[];
-  const trHead = document.getElementById('trhead') as HTMLTableCellElement;
-  (document.getElementById('caption') as Element).textContent = `${companies.length} applications`;
-
-  properties.forEach((property) => {
-    let th = document.createElement('th');
-    th.textContent = property.replace(/^\w/, m => m.toUpperCase()); // \w means [a-zA-Z_0-9]
-    // th.setAttribute('id', property);
-    th.addEventListener('click', (event) => printTableBody(event, property), false); // bubbling phase is default (false)
-    trHead.appendChild(th);
-  });
-  // document.getElementById('date').dispatchEvent(new MouseEvent('click'));
-  printTableBody(new MouseEvent('click'), 'date');
-  
-  function printTableBody(event: Event, column: Property): void {
-    // console.log(event?.type);
-    companies.sort((column === 'date') ? (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime() : (a, b) => a[column].localeCompare(b[column]));
-    // erase table body
-    const tableBody = document.getElementById('tablebody') as HTMLTableElement;
-    while (tableBody.firstChild !== null) {
-      tableBody.removeChild(tableBody.firstChild);
-    }
-    // generate table body
-    companies.forEach((company) => {
-      const tr = tableBody.insertRow();
-      properties.forEach((property) => {
-        tr.insertCell().textContent = (property === 'date') ? new Date(company.date).toLocaleDateString() : company[property];
-        // debugger;
-      })
-    })
-  }
+interface Company {
+  date: string
+  name: string
+  job: string
+  location: string
+  success: string
 }
+type Property = keyof Company
+const properties = ['date', 'name', 'job', 'location', 'success'] as Property[]
+const trHead = document.getElementById('trhead') as HTMLTableCellElement
 
-document.addEventListener('DOMContentLoaded', printTable);
+properties.forEach((property) => {
+  const th = document.createElement('th')
+  th.textContent = property.replace(/^\w/, m => m.toUpperCase()) // \w means [a-zA-Z_0-9]
+  th.setAttribute('id', property)
+  th.onclick = printTableBody
+  trHead.appendChild(th)
+});
+
+(document.getElementById('date') as Element).dispatchEvent(new MouseEvent('click'))
+
+async function printTableBody (event: Event): Promise<void> {
+  const response = await fetch('http://localhost:8080/companies.json')
+  const data = await response.json()
+  const companies: Company[] = data.companies;
+
+  (document.getElementById('caption') as Element).textContent = `${companies.length} applications`
+  const column = (event.target as Element).id as Property
+  companies.sort((column === 'date') ? (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime() : (a, b) => a[column].localeCompare(b[column]))
+  // erase table body
+  const tableBody = document.getElementById('tablebody') as HTMLTableElement
+  while (tableBody.firstChild !== null) {
+    tableBody.removeChild(tableBody.firstChild)
+  }
+  // generate table body
+  companies.forEach((company) => {
+    const tr = tableBody.insertRow()
+    properties.forEach((property) => {
+      tr.insertCell().textContent = (property === 'date') ? new Date(company.date).toLocaleDateString() : company[property]
+      // debugger;
+    })
+  })
+}
+// document.addEventListener('DOMContentLoaded', init)
